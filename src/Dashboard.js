318f97ba4a43391815559c6040e78bf5b8a7ea11
Shalini <ahana298@gmail.com>
2022-06-23 14:33:13 +0530

    Encryption, Decryption fixed

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,14 +4,11 @@ import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 import { auth, db, logout } from "./firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
-import ImageUploading from "react-images-uploading";
-import { storage } from "./firebase";
 import {
   ref,
   uploadBytesResumable,
   getDownloadURL,
   getStorage,
-  listAll,
 } from "firebase/storage";
 import { Encrypt } from "./Encrypt";
 import { Decrypt } from "./Decrypt";
@@ -19,61 +16,51 @@ import { Decrypt } from "./Decrypt";
 function Dashboard() {
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
-  const navigate = useNavigate();
-  const [images, setImages] = React.useState([]);
+
   const [rsaKey, setRsaKey] = useState("");
   const [aesKey, setAesKey] = useState("");
+  const [file, setFile] = useState("");
+
+  const navigate = useNavigate();
 
   // Decryption form
   const initalState = {
-    imgUrl: "",
+    cipherText: "",
     pwd: "",
     privateKey: "",
   };
 
   const [form, setForm] = useState(initalState);
 
-  const maxNumber = 69;
-  const [file, setFile] = useState("");
-  const [data, setData] = useState("");
-  const [inputKey, setInputKey] = useState("");
-  const [percent, setPercent] = useState(0);
   const storage = getStorage();
-  const listRef = ref(storage, "files/uid");
-
-  const onChange = (imageList, addUpdateIndex) => {
-    console.log(imageList, addUpdateIndex);
-    setImages(imageList);
-  };
+  ref(storage, "files/uid");
 
   function handleChange(event) {
     setFile(event.target.files[0]);
   }
 
   const handleEncrypt = () => {
-    let enc = Encrypt(file, aesKey, rsaKey);
-    alert(images[0]);
     const storageRef = ref(storage, "/files/" + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, enc);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const percent = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setPercent(percent);
-      },
-      (err) => console.log(err),
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          console.log({ url });
-        });
-      }
-    );
+    uploadBytesResumable(storageRef, file);
+
+    getDownloadURL(ref(storage, "/files/" + file.name))
+      .then(async (url) => {
+        const val = await Encrypt(url, aesKey, rsaKey);
+        setForm({ ...form, cipherText: val });
+      })
+      .catch((error) => {
+        console.error("Error in fetching url from firebase: " + error);
+      });
   };
 
-  const handleDecrypt = () => {
-    Decrypt(form.imgUrl, form.privateKey, form.pwd);
+  const handleDecrypt = async () => {
+    const decryptedUrl = await Decrypt(
+      form.cipherText,
+      form.pwd,
+      form.privateKey
+    );
+    const img = document.getElementById("myimg");
+    img.setAttribute("src", decryptedUrl);
   };
 
   const fetchUserName = async () => {
@@ -84,7 +71,6 @@ function Dashboard() {
       setName(data.name);
     } catch (err) {
       console.error(err);
-      //alert("An error occured while fetching user data");
     }
   };
 
@@ -95,7 +81,7 @@ function Dashboard() {
   }, [user, loading]);
 
   return (
-    <>
+    <div>
       <div className="header">
         <h1>Secure Image Sharing</h1>
         <p>Logged in as {name}</p>
@@ -103,166 +89,88 @@ function Dashboard() {
           Logout
         </button>
       </div>
-      <div className="dashboard">
+      <div className="dashboard" style={{ height: "100%" }}>
         <div className="dashboard__container">
           <h2> SEND </h2>
 
-          <ImageUploading
-            multiple
-            value={images}
-            onChange={onChange}
-            maxNumber={maxNumber}
-            dataURLKey="data_url"
-          >
-            {({
-              imageList,
-              onImageUpload,
-              onImageRemoveAll,
-              onImageUpdate,
-              onImageRemove,
-              isDragging,
-              dragProps,
-            }) => (
-              <div className="upload__image-wrapper">
-                <button
-                  className="dashboard__btn1"
-                  style={isDragging ? { color: "red" } : undefined}
-                  onClick={onImageUpload}
-                  {...dragProps}
-                >
-                  Preview Image
-                </button>
-                <br />
-                <br />
-                <label> Select File to Encrypt : </label>
-                <input
-                  type="file"
-                  id="img"
-                  onChange={handleChange}
-                  accept=".jpeg, .png, .jpg"
-                />
-                <br />
-                <label> Enter Password : </label>
-                <input
-                  type="text"
-                  id="ekey"
-                  value={aesKey}
-                  onChange={(e) => setAesKey(e.target.value)}
-                  placeholder="Enter AES Key"
-                />
-                <br />
-                <label> Enter Public Key : </label>
-                <input
-                  type="text"
-                  id="ekey1"
-                  value={rsaKey}
-                  onChange={(e) => setRsaKey(e.target.value)}
-                  placeholder="Enter RSA Key"
-                />
-                <br />
-                <br />
-                <button className="dashboard__btn1" onClick={handleEncrypt}>
-                  {" "}
-                  Encrypt File{" "}
-                </button>
-                {/*<p>{percent}% Encrypt Complete</p>
-                   <button className="dashboard__btn1" onClick={handleUpload}> Upload </button>
-                  <p>{percent}% Upload Complete</p>
-                  <br />
-                  {imageList.map((image, index) => (
-                    <div key={index} className="image-item">
-                      <img src={image['data_url']} alt="" width="100" />
-                      <div className="image-item__btn-wrapper">
-                        <button onClick={() => onImageUpdate(index)}>Update</button>
-                        <button onClick={() => onImageRemove(index)}>Remove</button>
-                      </div>
-                    </div>
-                  ))} */}
-              </div>
-            )}
-          </ImageUploading>
+          <div className="upload__image-wrapper">
+            <img
+              id="myimg"
+              src="https://www.lankabangla.com/wp-content/uploads/2019/08/no_image_png_935227.png"
+              alt="After Decrypting"
+              style={{ height: "60px", width: "70px" }}
+            />
+            <br />
+            <br />
+            <label> Select File to Encrypt : </label>
+            <input
+              type="file"
+              id="img"
+              onChange={handleChange}
+              accept=".jpeg, .png, .jpg"
+            />
+            <br />
+            <label> Enter Password : </label>
+            <input
+              type="text"
+              id="ekey"
+              value={aesKey}
+              onChange={(e) => setAesKey(e.target.value)}
+              placeholder="Enter AES Key"
+            />
+            <br />
+            <label> Enter Public Key : </label>
+            <input
+              type="text"
+              id="ekey1"
+              value={rsaKey}
+              onChange={(e) => setRsaKey(e.target.value)}
+              placeholder="Enter RSA Key"
+            />
+            <br />
+            <br />
+            <button className="dashboard__btn1" onClick={handleEncrypt}>
+              {" "}
+              Encrypt File{" "}
+            </button>
+          </div>
         </div>
         <div className="dashboard__container1">
           <h2> RECEIVE </h2>
 
-          <ImageUploading
-            multiple
-            value={images}
-            onChange={onChange}
-            maxNumber={maxNumber}
-            dataURLKey="data_url"
-          >
-            {({
-              imageList,
-              onImageUpload,
-              onImageRemoveAll,
-              onImageUpdate,
-              onImageRemove,
-              isDragging,
-              dragProps,
-            }) => (
-              <div className="upload__image-wrapper">
-                <button
-                  className="dashboard__btn1"
-                  style={isDragging ? { color: "red" } : undefined}
-                  onClick={onImageUpload}
-                  {...dragProps}
-                >
-                  Preview Image
-                </button>
-                <br />
-                <br />
-                <label> Select File to Decrypt : </label>
-                <input
-                  type="text"
-                  value={form.imgUrl}
-                  onChange={(e) => setForm({ ...form, imgUrl: e.target.value })}
-                />
-                <br />
-                <label> Enter Password : </label>
-                <input
-                  type="text"
-                  id="dkey"
-                  value={form.pwd}
-                  onChange={(e) => setForm({ ...form, pwd: e.target.value })}
-                  placeholder="Enter Key"
-                />
-                <br />
-                <label> Enter Private Key : </label>
-                <input
-                  type="text"
-                  id="dkey1"
-                  value={form.privateKey}
-                  onChange={(e) =>
-                    setForm({ ...form, privateKey: e.target.value })
-                  }
-                  placeholder="Enter Key"
-                />
-                <br />
-                <br />
-                <button className="dashboard__btn1" onClick={handleDecrypt}>
-                  {" "}
-                  Decrypt File{" "}
-                </button>
-                {/*<p>{percent}% Decrypt Complete</p>
-                   <button className="dashboard__btn1" onClick={handleDownload}> Download </button>
-                  <p>{percent}% Download Complete</p>
-                  <br />
-                  {imageList.map((image, index) => (
-                    <div key={index} className="image-item">
-                      <img src={image['data_url']} alt="" width="100" />
-                      <div className="image-item__btn-wrapper">
-                        <button onClick={() => onImageUpdate(index)}>Update</button>
-                        <button onClick={() => onImageRemove(index)}>Remove</button>
-                      </div>
-                    </div>
-                  ))} */}
-              </div>
-            )}
-          </ImageUploading>
+          <div className="upload__image-wrapper">
+            <br />
+            <br />
+            <label> Select File to Decrypt : </label>
+            <input type="text" value={form.cipherText} />
+            <br />
+            <label> Enter Password : </label>
+            <input
+              type="text"
+              id="dkey"
+              value={form.pwd}
+              onChange={(e) => setForm({ ...form, pwd: e.target.value })}
+              placeholder="Enter Key"
+            />
+            <br />
+            <label> Enter Private Key : </label>
+            <input
+              type="text"
+              id="dkey1"
+              value={form.privateKey}
+              onChange={(e) => setForm({ ...form, privateKey: e.target.value })}
+              placeholder="Enter Key"
+            />
+            <br />
+            <br />
+            <button className="dashboard__btn1" onClick={handleDecrypt}>
+              {" "}
+              Decrypt File{" "}
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Decrypt.js
+++ src/Decrypt.js
@@ -1,36 +1,15 @@
 import CryptoJS from "crypto-js";
-import FileSaver from "file-saver";
 import { JSEncrypt } from "jsencrypt";
 
-export const Decrypt = async(file, inputKey, inputKey1) => {
-    console.log(file);
-    console.log(inputKey);
-    console.log(inputKey1);
-    var decryption = new JSEncrypt();
+export const Decrypt = async (url, inputKey, inputKey1) => {
+  // AES Decryption
+  const bytes = CryptoJS.AES.decrypt(url, inputKey);
+  const firstLevelCipher = bytes.toString(CryptoJS.enc.Utf8);
 
-    const callDecrypt=(arg)=>{
-        console.log("Img-Base64:",arg); 
-        console.log("AES Key",inputKey);
-        console.log("RSA Key",inputKey1);
-        decryption.setPrivateKey(inputKey1);
-        const ct = CryptoJS.AES.decrypt(arg, inputKey).toString(CryptoJS.enc.Utf8);
-        console.log("AES Done",ct);
-        const rsaData = decryption.decrypt(ct);
-        console.log("RSA Done",rsaData);
-        //dataurl to image conversion
-        var blob = new Blob([rsaData],{type:"image/jpeg"});
-        console.log(blob);
-        var blob1 = new Blob([rsaData], {type:"plain/text"});
-        console.log(blob1);
-        FileSaver.saveAs(blob,"decryptedFile.jpg");
-        FileSaver.saveAs(blob1,"decrypt.txt");
-    }
+  // RSA Decryption
+  const decryption = new JSEncrypt();
+  decryption.setPrivateKey(inputKey1);
+  const originalText = decryption.decrypt(firstLevelCipher);
 
-    let fread = new FileReader();
-    fread.onloadend = () => {
-        console.log(fread);
-        callDecrypt(fread);
-    }
-    fread.readAsText(file);
-    console.log("fread",fread);
-}
+  return originalText;
+};
